0ce3b7ce8b4e9a9aedb215388c8cc5e61ddf9c3a
Jonathan Leek <jonathanleek@gmail.com>
2020-07-10 21:44:16 -0500

    Setting up project, importing geotags

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # geophoto
 Python program for reverse geocoding images based on exif data and sorting into folders by address
+Built based on https://developer.here.com/blog/getting-started-with-geocoding-exif-image-metadata-in-python3
---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from PIL import Image
+from PIL.ExifTags import TAGS
+from PIL.ExifTags import GPSTAGS
+
+
+pic_location = ""
+def get_exif(filename):
+    image = Image.open(filename)
+    image.verify()
+    return image._getexif()
+
+def get_labeled_exif(exif):
+    labeled = {}
+    for (key, val) in exif.items():
+        labeled[TAGS.get(key)] = val
+
+    return labeled
+
+def get_geotagging(exif):
+    if not exif:
+        raise ValueError("No EXIF metadata found")
+
+    geotagging = {}
+    for (idx, tag) in TAGS.items():
+        if tag == 'GPSInfo':
+            if idx not in exif:
+                raise ValueError("No EXIF geotagging found")
+
+            for (key, val) in GPSTAGS.items():
+                if key in exif[idx]:
+                    geotagging[val] = exif[idx][key]
+
+    return geotagging
+
+
+
+exif = get_exif(pic_location)
+geotags = get_geotagging(exif)
+print(geotags)
